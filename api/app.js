276197ed65b0c3df9ef5d00324138209c5eb5b08
Your Name <your.email@example.com>
2024-09-16 10:35:19 +0800

    Update app.js

--- api/app.js
+++ api/app.js
@@ -27,5 +27,5 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${allowedOrigins} ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
